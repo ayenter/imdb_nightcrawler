8352c8b7761b4b3808b08e4b83b64e1fd735c10c
Alec Yenter <alecyenter@csu.fullerton.edu>
2017-04-30 19:47:27 -0500

    regresion -> classes

--- create_dataset.py
+++ create_dataset.py
@@ -95,7 +95,7 @@ def create_dataset_y(data_y, folder):
 	bar = progressbar.ProgressBar()
 	print("Generating labels lmdb for " + folder)
 	for i in bar(range(len(data_y))):
-		datum = caffe.io.array_to_datum(data_y[i].astype('int8').reshape((1,1,1)))
+		datum = caffe.io.array_to_datum(data_y[i].astype('int').reshape((1,1,1)))
 		batch.append(('{:0>10d}'.format(i+1), datum))
 		if len(batch) >= db_batch_size:
 				_write_batch_to_lmdb(output_db, batch)
